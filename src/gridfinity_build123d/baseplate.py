2c40f4bc336a8347680c67c5ad3a441f2d042ea7
Eemeli Ranta <rantaeemeli@gmail.com>
2026-03-19 18:21:59 +0200

    Refactor functionality to outside of BasePlate

--- src/gridfinity_build123d/baseplate.py
+++ src/gridfinity_build123d/baseplate.py
@@ -16,17 +16,57 @@ from build123d import (
     BuildPart,
     Edge,
     Mode,
+    Part,
     RotationLike,
     Shape,
     fillet,
 )
 
 from gridfinity_build123d.baseplate_block import BasePlateBlockFrame
+from gridfinity_build123d.constants import gridfinity_standard
 from gridfinity_build123d.utils import Utils
 
 if TYPE_CHECKING:
     from gridfinity_build123d.baseplate_block import BasePlateBlock
     from gridfinity_build123d.features import Feature
+
+
+def _normalize_features(features: Feature | list[Feature] | None) -> list[Feature]:
+    """Normalize optional feature input into a concrete list."""
+    if not features:
+        return []
+
+    return features if isinstance(features, Iterable) else [features]
+
+
+def _round_baseplate_outer_edges(part: BuildPart) -> Part:
+    """Apply fillet to outer edges of a baseplate.
+
+    Args:
+        part (BuildPart): The BuildPart containing the baseplate to fillet.
+    """
+    if not part.part:  # pragma: no cover
+        msg = "Part is empty"
+        raise RuntimeError(msg)
+
+    z_height = part.part.bounding_box().size.Z
+
+    def edge_filter(shape: Shape[Edge]) -> bool:
+        inner_edge = shape.edge()
+        if not inner_edge:  # pragma: no cover
+            m = "Edge is empty"
+            raise RuntimeError(m)
+
+        return isclose(inner_edge.length, z_height)
+
+    wires = part.edges().filter_by(Axis.Z).filter_by(edge_filter)
+
+    return fillet(wires, gridfinity_standard.grid.radius)
+
+
+def _create_equal_grid(size_x: int, size_y: int) -> list[list[bool]]:
+    """Create a grid of specified size with all values set to True."""
+    return [[True] * size_x for _ in range(size_y)]
 
 
 class BasePlate(BasePartObject):
@@ -58,30 +98,12 @@ class BasePlate(BasePartObject):
         if baseplate_block is None:
             baseplate_block = BasePlateBlockFrame()
 
-        if not features:
-            features = []
-
-        self.features: list[Feature] = features if isinstance(features, Iterable) else [features]
+        self.features: list[Feature] = _normalize_features(features)
 
         with BuildPart() as part:
             _ = Utils.place_by_grid(baseplate_block.create_obj(mode=Mode.PRIVATE), grid)
 
-            if not part.part:  # pragma: no cover
-                msg = "Part is empty"
-                raise RuntimeError(msg)
-
-            z_height = part.part.bounding_box().size.Z
-
-            def edge_filter(shape: Shape[Edge]) -> bool:
-                inner_edge = shape.edge()
-                if not inner_edge:  # pragma: no cover
-                    m = "Edge is empty"
-                    raise RuntimeError(m)
-
-                return isclose(inner_edge.length, z_height)
-
-            wires = part.edges().filter_by(Axis.Z).filter_by(edge_filter)
-            _ = fillet(wires, 4)
+            _ = _round_baseplate_outer_edges(part)
 
             for feature in self.features:
                 feature.apply(part)
@@ -120,5 +142,5 @@ class BasePlateEqual(BasePlate):
         if baseplate_block is None:
             baseplate_block = BasePlateBlockFrame()
 
-        grid: list[list[bool]] = [[True] * size_x for _ in range(size_y)]
+        grid = _create_equal_grid(size_x, size_y)
         super().__init__(grid, baseplate_block, features, rotation, align, mode)
